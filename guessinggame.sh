8e1dcfbca0a0678b4920562e68473545ab329faf
Sam Feng <17917969+wr-fenglei@users.noreply.github.com>
2020-01-30 02:47:48 +0800

    add compare function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
-function filesCount() {
-    count=$(ls -l | grep ^- | wc -l)
-    echo $count
-}
-
-count=$(filesCount)
-echo "Can you guess how many files are in the current directory?"
-ret=0
-while [[ $ret -eq 0 ]]
-do
-    read user_answer
-    if [[ $user_answer -gt $count ]]
+function compare() {
+    if [[ $1 -gt $2 ]]
     then
         echo "Your guess was too high, please try to guess again."
-    elif [[ $user_answer -lt $count ]]
+    elif [[ $1 -lt $2 ]]
     then
         echo "Your guess was too low, please try to guess again."
     else
-        let ret=1
         echo "Congratulation! Your guess was correct."
     fi
+}
+
+count=$(ls -l | grep ^- | wc -l)
+echo "Can you guess how many files are in the current directory?"
+while [[ $user_answer -ne $count ]]
+do
+    read user_answer
+    echo $(compare $user_answer $count)
 done
