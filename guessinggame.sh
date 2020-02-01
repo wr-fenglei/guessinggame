43907094a6ec80361209e170ed12b4cc752c57e2
Sam Feng <17917969+wr-fenglei@users.noreply.github.com>
2020-02-02 03:54:23 +0800

    add is_integer function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,28 @@
-#!/usr/bin/env bash
 function count() {
-    count=$(ls -l | grep ^- | wc -l)
-    echo $count
+    echo $(ls -al | grep ^- | wc -l)
+}
+
+function is_integer() {
+    [[ $1 =~ ^0$|^[+-]?[1-9][0-9]*$ ]] && true || false
 }
 
 function compare() {
-    if [[ $1 -gt $2 ]]
-    then
+    if [[ $1 -gt $2 ]]; then
         echo "Your guess was too high, please try to guess again."
-    elif [[ $1 -lt $2 ]]
-    then
+    elif [[ $1 -lt $2 ]]; then
         echo "Your guess was too low, please try to guess again."
     else
         echo "Congratulation! Your guess was correct."
     fi
 }
 
-count=$(count)
 echo "Can you guess how many files are in the current directory?"
-while [[ $user_answer -ne $count ]]
-do
-    read user_answer
-    echo $(compare $user_answer $count)
+count=$(count)
+while ! ($(is_integer $answer) && [[ $answer -eq $count ]]); do
+    read answer
+    if ! $(is_integer $answer); then
+        echo "Your guess was not integer, please try to guess again."
+    else
+        echo $(compare $answer $count)
+    fi
 done
